780d53994b3f011b7465533c6e7e9b6f838194c0
Mslick <nanthapopguitarist@gmail.com>
2022-10-02 13:56:50 +0700

    up

--- Function/TEST.c
+++ Function/TEST.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
 void main(){
-    int N;
-    double sum = 0;
-    double Average;
-    scanf("%d", &N);
-    sum = (N / 8);
-    printf("%.2lf", sum);
-    return 0;
+    printf("HI GOOD");
 }
